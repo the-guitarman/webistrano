125eef112b70d9e4a15c14f4837d70384f4b0c46
Felipe Godoy <felipegodoy@id.uff.br>
2012-10-29 16:30:28 -0200

    criados testes de criação de host

---
+++ features/step_definitions/criar_host_steps.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+Dado /^que o administrador visite a página de criação de hosts$/ do
+  visit new_host_path
+end
+
+Quando /^o administrador preenche os campos de criação de hosts com dados coerentes$/ do
+  fill_in "host_name", "novo_host"
+end
+
+Então /^a aplicação direciona para a página do novo host$/ do
+  visit host_path(Host.last)
+end
