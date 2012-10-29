d54272d92fd508b8d52e7ffe98fd99a9e7a1f9fe
Lucas Nadalutti <lucasnadalutti@gmail.com>
2012-10-29 17:40:04 -0200

    Concluindo testes de criacao de projeto

--- features/admin/criar_projeto.feature
+++ features/admin/criar_projeto.feature
@@ -7,16 +7,19 @@ Funcionalidade: Administrador cria um novo projeto
 
   Cenário: Sucesso na criação do projeto
     Quando o usuário preenche os campos de criação de projeto corretamente
+    E o usuário clica no botão "Create Project"
     Então o usuário deve estar na página de visualização daquele projeto
     E o usuário deve ver uma mensagem de sucesso na criação do projeto
 
   Cenário: Falha na criação do projeto (sem nome)
     Quando o usuário preenche os campos de criação de projeto sem preencher o nome
+    E o usuário clica no botão "Create Project"
     Então o usuário deve estar na página de criação de projetos
     E o usuário deve ver uma mensagem de nome do projeto obrigatório
 
   Cenário: Falha na criação do projeto (nome já existente)
     Quando o usuário preenche os campos de criação de projeto com um nome já existente
+    E o usuário clica no botão "Create Project"
     Então o usuário deve estar na página de criação de projetos
     E o usuário deve ver uma mensagem de nome do projeto já existente
 
--- features/step_definitions/criar_projeto_steps.rb
+++ features/step_definitions/criar_projeto_steps.rb
@@ -3,3 +3,48 @@
 Dado /^que o administrador visite a página de criação de projetos$/ do
   visit new_project_path
 end
+
+Quando /^o usuário preenche os campos de criação de projeto corretamente$/ do
+  fill_in "project_name", with: "Teste"
+  select "Mod Rails", from: "project_template"
+  fill_in "project_description", with: "Projeto para testes Cucumber"
+end
+
+Então /^o usuário deve estar na página de visualização daquele projeto$/ do
+  page.should have_content "Project Teste"
+  page.should have_content "Edit project"
+  page.should have_content "Clone project"
+  page.should have_content "Delete project"
+end
+
+Então /^o usuário deve ver uma mensagem de sucesso na criação do projeto$/ do
+  page.should have_content "Project was successfully created."
+end
+
+Quando /^o usuário preenche os campos de criação de projeto sem preencher o nome$/ do
+  select "Mod Rails", from: "project_template"
+  fill_in "project_description", with: "Projeto para testes Cucumber"
+end
+
+Então /^o usuário deve estar na página de criação de projetos$/ do
+  page.should have_content "New project"
+end
+
+Então /^o usuário deve ver uma mensagem de nome do projeto obrigatório$/ do
+  page.should have_content "Name can't be blank"
+end
+
+Quando /^o usuário preenche os campos de criação de projeto com um nome já existente$/ do
+  project = FactoryGirl.create :project
+  fill_in "project_name", with: project.name
+  select "Mod Rails", from: "project_template"
+  fill_in "project_description", with: "Projeto para testes Cucumber"
+end
+
+Então /^o usuário deve ver uma mensagem de nome do projeto já existente$/ do
+  page.should have_content "Name has already been taken"
+end
+
+Então /^o usuário deve estar na lista de visualização dos projetos$/ do
+  current_path.should == projects_path
+end
