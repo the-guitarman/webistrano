2bd8acf44fdc8dba0fbe3c185299ce292726549f
Lucas Nadalutti <lucasnadalutti@gmail.com>
2012-10-29 15:51:48 -0200

    Adicionando mais steps comuns

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -3,3 +3,11 @@
 Quando /^o (usuário|administrador) clica no botão "([^']*)"$/ do |obj, botao|
   click_button botao
 end
+
+Quando /^o (usuário|administrador) clica em "([^']*)"$/ do |obj, link|
+  click_link link
+end
+
+Dado /^que o usuário esteja logado$/ do
+  login @usuario
+end
