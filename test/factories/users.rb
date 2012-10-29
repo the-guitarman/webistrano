27d0774f4c52fa0f6363345205e3824ae0ca4c5d
Felipe Godoy <felipegodoy@id.uff.br>
2012-10-29 14:40:39 -0200

    adicionado cucumber do login

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -3,7 +3,14 @@ FactoryGirl.define do
     sequence(:login) { |n| "user_%04d" % n }
     sequence(:email) { |n| "user_%04d@example.com" % n }
     admin       false
-    password    'hello!'
+    password    '123456'
     password_confirmation { |a| a.password }
   end
+
+  factory :admin, parent: :user do
+    sequence(:login) { |n| "admin_%04d" % n }
+    sequence(:email) { |n| "admin_%04d@example.com" % n }
+    admin       true
+  end
+
 end
