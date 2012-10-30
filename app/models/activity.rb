81f2e279afa9b6605760a7b28e7a700e7d70cf24
Lucas Nadalutti <lucasnadalutti@gmail.com>
2012-10-30 15:23:51 -0200

    Atualizando codigo

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,7 +2,7 @@ class Activity < ActiveRecord::Base
   attr_accessible :user_id, :target_id, :target_type, :tag, :data
 
   belongs_to :user
-  belongs_to :target, :polymorphic => true
+  belongs_to :target, polymorphic: true
 
   serialize :data
 
