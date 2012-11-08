5fa8721254be0301e88e2b6ab24ff6aac4b1b9c7
Felipe Godoy <felipegodoy@id.uff.br>
2012-11-08 11:32:21 -0200

    adicionada logo sti

--- features/step_definitions/permissoes_projeto_step.rb
+++ features/step_definitions/permissoes_projeto_step.rb
@@ -4,9 +4,10 @@ Então /^o seletor deve conter o usuário$/ do
   page.should have_select("project_user", options: [@usuario.name])
 end
 
-Então /^a página (não )?deve conter o link do usuário inserido na sessão de usuários do projeto$/ do |negacao|
+Então /^a página (não )?deve conter o usuário inserido na sessão de usuários do projeto$/ do |negacao|
+  save_and_open_page
   within("#project_users_list") do
-    negacao ? (page.should_not have_link(@usuario.name)) : (page.should have_link(@usuario.name))
+    negacao ? (page.should_not have_link("Profile")) : (page.should have_link("Profile"))
   end
 end
 
@@ -21,5 +22,11 @@ end
 Então /^a página não deve conter o link do administrador inserido na sessão de usuários do projeto$/ do
   within("#project_users_list") do
     page.should_not have_link @outro_admin.name
+  end
+end
+
+Quando /^o administrador clica em Delete sessão de usuários$/ do
+  within("#project_users_list") do
+    click_link "Delete"
   end
 end
