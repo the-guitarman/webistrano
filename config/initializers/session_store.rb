5e745c1a739b0c42879c71037aedf391fac5e9ce
sebastian <sebastian@cairns>
2013-06-30 01:04:58 +0200

    session store secret

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Webistrano::Application.config.session_store :cookie_store, :key => '_webistrano_session'
+Webistrano::Application.config.session_store :cookie_store, 
+  :key => '_webistrano_session',
+  :secret => 'please choose a long random string, min. 128 characters'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
